0142996c2d8b461f8f36a5b5bb2658a553cd84ea
markusbuchholz <markus.x.buchholz@gmail.com>
2025-08-30 17:40:45 +0100

    Udpate

---
+++ src/robot_senor.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <memory>
+
+// Abstract base class (interface for all sensors)
+class Sensor {
+public:
+    virtual void readData() = 0; // pure virtual → must be implemented
+    virtual ~Sensor() = default; // virtual destructor (important!)
+
+    virtual int compute(int a, int b) { 
+        std::cout << "Default compute\n"; 
+        return 0; 
+    }
+};
+
+// Derived class: Camera sensor
+class Camera : public Sensor {
+public:
+    void readData() override {
+        std::cout << "Camera capturing image...\n";
+    }
+};
+
+// Derived class: IMU sensor
+class IMU : public Sensor {
+public:
+    void readData() override {
+        std::cout << "IMU measuring acceleration and orientation...\n";
+    }
+};
+
+// Derived class: Sonar sensor
+class Sonar : public Sensor {
+public:
+    void readData() override {
+        std::cout << "Sonar detecting distance to obstacle...\n";
+    }
+
+    int compute (int a, int b)override{
+        std::cout << "computing :" << std::endl;
+
+        return  a * b;
+    }
+};
+
+int main() {
+    // Polymorphism: base class pointer to different derived objects
+    std::unique_ptr<Sensor> s1 = std::make_unique<Camera>();
+    std::unique_ptr<Sensor> s2 = std::make_unique<IMU>();
+    std::unique_ptr<Sensor> s3 = std::make_unique<Sonar>();
+
+    s1->readData(); // "Camera capturing image..."
+    s2->readData(); // "IMU measuring acceleration and orientation..."
+    s3->readData(); // "Sonar detecting distance to obstacle..."
+ 
+    s3->compute(10,20);
+    
+    std::unique_ptr<Sensor> sens;
+
+    sens = std::make_unique<Camera>();
+    sens->readData();
+
+    sens = std::make_unique<IMU>();
+    sens->readData();
+}
